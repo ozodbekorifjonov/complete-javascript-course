eb02dcfdb431dd68bca6b5a60571f9ab94ec34e2
Ozodbek Oripjonov <ozod3837898@gmail.com>
2021-12-13 17:14:44 +0500

    done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,57 @@
+'use strict'
+
+let a = false;
+const b = true;
+
+if (b) a = true;
+if (a) console.log('yes');
+
+//function declaration
+const age1 = getAge1(1998);
+
+function getAge1(birthYear) {
+    return 2021 - birthYear;
+}
+
+// function expression
+
+// const age2 = calcAge2(1998) it is NOT allowed to call there 🚫
+
+const calcAge2 = function (birthYear) {
+    return 2021 - birthYear;
+}
+
+const age2 = calcAge2(1998);
+
+const calcAge3 = birthYear => 2021 - birthYear;
+
+const age3 = calcAge3(1998);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} left ${retirement} years`;
+}
+
+const retirementYear = yearsUntilRetirement(1998, 'Bob');
+
+// console.log(retirementYear);
+
+function getName(name) {
+    return `Hello ${name}!`;
+    console.log('Is it working?')
+}
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(123, 134, 127);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        return `Dolphins win (${avgDolphins} vs ${avgKoalas})`;
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        return `Koalas win (${avgKoalas} vs ${avgDolphins})`;
+    } else return `No one won the game 😒`;
+}
+
+console.log(checkWinner(avgDolphins, avgKoalas));
